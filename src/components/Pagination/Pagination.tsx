f06ad0df045c18e9f09e2d43b73b9104cc437bc3
Mykhailo Forkosh <forkos277@gmail.com>
2025-01-27 22:13:23 +0200

    test

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,65 @@
-export const Pagination = () => {};
+import React from 'react';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage?: number;
+  onPageChange: (v: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage = 1,
+  onPageChange,
+}) => {
+  const numberOfPage: React.JSX.Element[] = [];
+
+  for (let i = 1; i <= Math.ceil(total / perPage); i++) {
+    const a = (
+      <li
+        className={`page-item ${currentPage === i ? 'active' : ''}`}
+        onClick={() => onPageChange(i)}
+        key={i}
+      >
+        <a data-cy="pageLink" className="page-link" href={`#${i}`}>
+          {i}
+        </a>
+      </li>
+    );
+
+    numberOfPage.push(a);
+  }
+
+  return (
+    <>
+      <ul className="pagination">
+        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1 && 'true'}
+            onClick={() => onPageChange(currentPage - 1)}
+          >
+            «
+          </a>
+        </li>
+        {numberOfPage}
+        <li
+          className={`page-item ${currentPage === numberOfPage.length ? 'disabled' : ''}`}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === numberOfPage.length && 'true'}
+            onClick={() => onPageChange(currentPage + 1)}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
